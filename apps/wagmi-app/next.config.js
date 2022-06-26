f973bd0ab8f60d5830a47ddd2271ec874d5ac9b3
JustBassey <justinbassey@gmail.com>
2022-06-26 08:04:08 -0400

    config for export

--- apps/wagmi-app/next.config.js
+++ apps/wagmi-app/next.config.js
@@ -1,18 +1,19 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 });
 
 module.exports = withBundleAnalyzer({
   eslint: {
-    dirs: ['.'],
+    dirs: ["."],
   },
   ignoreBuildErrors: true,
   poweredByHeader: false,
   trailingSlash: true,
-  basePath: '',
+  basePath: "",
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
+  assetPrefix: "./",
 });
